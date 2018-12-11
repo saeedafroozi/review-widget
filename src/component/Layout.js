6da39ee4aae1947fcb5af0658c2a72d301305600
afroozi <afroozi@AFROOZI-PC>
2018-12-11 18:53:10 +0330

    delete extra references

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
 class Layout extends Component {
-    validate() {
-        React.Children.map(this.props.children, (child, index) => {
-            if (child.type.name !== 'Row') {
-                throw "The First Child Must Be RowComponent"
-            }
-        });
-    }
+  
     
     render() {
-        this.validate();
+      
 
         return <div className='container'>
             {this.props.children}
